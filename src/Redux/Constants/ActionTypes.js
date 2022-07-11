f6b08d4eb06265805b2d26a98e2a2b38d6c07b6d
Hitkumar <hit98987@gmail.com>
2022-07-11 17:02:01 +0200

    initial commit

---
+++ src/Redux/Constants/ActionTypes.js
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+// const ActionTypes = () => {
+//   return (
+//     <div>ActionTypes</div>
+//   )
+// }
+
+// export default ActionTypes
+
+export const ActionTypes = {
+  SET_PRODUCTS: "SET_PRODUCTS",
+  SELECTED_PRODUCT: "SELECTED_PRODUCT",
+  REMOVE_SELECTED_PRODUCT: "REMOVE_SELECTED_PRODUCT",
+};
